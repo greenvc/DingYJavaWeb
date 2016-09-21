f65c9b0974ad3ea260e3578c1e2d05edf1424ec9
greenvc <greenvc@126.com>
2016-09-21 16:15:05 +0800

    基于Java Web开发技术的示例代码

---
+++ src/DingY/DB/SqlDB.java
@@ -0,0 +1,9 @@
+package DingY.DB;
+
+/*
+ * Sql Server 数据访问类
+ */
+public class SqlDB {
+	public SqlDB(){
+	}
+}
---
+++ src/DingY/Securitys/MD5Encrypt.java
@@ -0,0 +1,10 @@
+package DingY.Securitys;
+
+/*
+ * MD5加密类
+ */
+public class MD5Encrypt {
+	public MD5Encrypt(){
+		
+	}
+}
