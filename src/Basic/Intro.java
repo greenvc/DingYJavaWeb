8ec1fe87a49db088f5dee9fe1a138cc2ba485d82
greenvc <greenvc@126.com>
2017-10-09 22:29:00 +0800

    第一个Java程序

---
+++ src/Basic/Intro.java
@@ -0,0 +1,11 @@
+package Basic;
+
+public class Intro {
+	/*
+	 * 第一个Java程序
+	 */
+	public static void main(String[] args)
+	{
+		System.out.print("我的Java学习之旅开始了");
+	}
+}
--- src/DingY/DB/SqlDB.java
+++
@@ -1,9 +0,0 @@
-package DingY.DB;
-
-/*
- * Sql Server 数据访问类
- */
-public class SqlDB {
-	public SqlDB(){
-	}
-}
