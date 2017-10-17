f093508123e39061f63d29d8f43c16b03aa7d685
greenvc <greenvc@126.com>
2017-10-17 13:27:17 +0800

    使用异常

---
+++ src/Exceptions/UseException.java
@@ -0,0 +1,25 @@
+package Exceptions;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+
+public class UseException {
+
+	public static void main(String[] args) {
+		System.out.println("请输入一个整数字符串:");
+		try{
+			BufferedReader objBufferedReader = new BufferedReader(new InputStreamReader (System.in));
+			int a = Integer.parseInt(objBufferedReader.readLine());
+			System.out.println("您输入的整数是：  "+a);    
+		} 
+		catch(IOException ExIO) { 
+			System.out.println("IO错误。。。"); 
+		}
+		catch(NumberFormatException ex)
+		{
+			System.out.println("您输入的不是一个整数字符串，");
+		}
+	}
+}
