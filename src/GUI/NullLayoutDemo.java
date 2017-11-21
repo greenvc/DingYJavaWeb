ea8b19b7be3eaced7a1317fe45ee9fbb6ad85be1
greenvc <greenvc@126.com>
2017-11-21 19:03:06 +0800

    自由布局

---
+++ src/GUI/NullLayoutDemo.java
@@ -0,0 +1,33 @@
+package GUI;
+
+import java.awt.*;
+import javax.swing.*;
+ 
+public class NullLayoutDemo extends JFrame{
+	JButton btn1,btn2;
+	 
+	NullLayoutDemo() {
+		this.setTitle("自由布局");
+		this.setBounds(100,100,250,150);
+
+		//设置窗体为空布局
+		this.setLayout(null);
+		btn1=new JButton("按钮A");
+		btn2=new JButton("按钮B");
+
+		this.add(btn1);
+		//设置按钮btn1的精确位置
+		btn1.setBounds(30,30,80,25);
+
+		this.add(btn2);
+		btn2.setBounds(150,40,80,25);
+
+		this.setVisible(true);
+		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		this.setLocationRelativeTo(null);//让窗体居中显示
+	}
+
+	public static void main(String args[]){
+		new NullLayoutDemo();
+	}
+}
