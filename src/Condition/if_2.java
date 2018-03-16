15324ee601902ff593a6339577a14e1fddfc2147
greenvc <greenvc@126.com>
2018-03-16 11:26:34 +0800

    if嵌套语句

---
+++ src/Condition/if_2.java
@@ -0,0 +1,19 @@
+package Condition;
+
+/**
+ * if嵌套语句
+ * @author dingy
+ *
+ */
+public class if_2 {
+
+	public static void main(String[] args) {
+		int x = 30;
+	    int y = 10;
+	    if( x == 30 ){
+	    	if( y == 10 ){
+	    		System.out.print("X = 30 and Y = 10");
+	    	}
+	    }
+	}
+}
