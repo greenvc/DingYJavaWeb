28d6c4e68bf0eb38df9b9e5c537d85e3d4ee3b8c
greenvc <greenvc@126.com>
2018-03-16 15:15:48 +0800

    逻辑运算符

---
+++ src/Basic/Logic.java
@@ -0,0 +1,18 @@
+package Basic;
+
+/**
+ * 逻辑运算符
+ * @author dingy
+ *
+ */
+public class Logic {
+
+	public static void main(String[] args) {
+	     boolean a = true;
+	     boolean b = false;
+	     System.out.println("a && b = " + (a&&b));
+	     System.out.println("a || b = " + (a||b) );
+	     System.out.println("!(a && b) = " + !(a && b));
+	}
+
+}
