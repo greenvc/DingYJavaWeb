8b75ca9a122e573844a0e078665fc5d1b06603f2
greenvc <greenvc@126.com>
2018-03-16 11:23:02 +0800

    if else语句

---
+++ src/Condition/ifelse.java
@@ -0,0 +1,24 @@
+package Condition;
+
+/**
+ * if else语句
+ * @author dingy
+ *
+ */
+public class ifelse {
+
+	public static void main(String[] args) {
+		int x = 30;
+		 
+	      if( x == 10 ){
+	         System.out.print("Value of X is 10");
+	      }else if( x == 20 ){
+	         System.out.print("Value of X is 20");
+	      }else if( x == 30 ){
+	         System.out.print("Value of X is 30");
+	      }else{
+	         System.out.print("这是 else 语句");
+	      }
+	}
+
+}
