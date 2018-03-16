dd683e2dac231599a36a758abb47174405b50f20
greenvc <greenvc@126.com>
2018-03-16 11:58:02 +0800

    自增自减运算符

---
+++ src/Basic/selfAddMinus.java
@@ -0,0 +1,19 @@
+package Basic;
+
+/**
+ * 自增（++）自减（--）运算符是一种特殊的算术运算符，在算术运算符中需要两个操作数来进行运算，而自增自减运算符是一个操作数。
+ * @author dingy
+ *
+ */
+public class selfAddMinus {
+
+	public static void main(String[] args) {
+        int a = 3;//定义一个变量；
+        int b = ++a;//自增运算
+        int c = 3;
+        int d = --c;//自减运算
+        System.out.println("进行自增运算后的值等于"+b);
+        System.out.println("进行自减运算后的值等于"+d);
+	}
+
+}
