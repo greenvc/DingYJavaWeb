bd798975fc772dc287279ea57c4e822f4fd61b95
greenvc <greenvc@126.com>
2018-03-16 11:20:39 +0800

    条件语句

---
+++ src/Condition/If_1.java
@@ -0,0 +1,18 @@
+package Condition;
+
+/**
+ * 条件语句，判断变量x是否比20大
+ * @author dingy
+ *
+ */
+public class If_1 {
+
+	public static void main(String[] args) {
+	      int x = 10;
+	      
+	      if( x < 20 ){
+	         System.out.print("X比20小！");
+	      }
+	}
+
+}
