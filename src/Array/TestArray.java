6da7e96915bed958f1ca31b48cc94221ff6a09a9
greenvc <greenvc@126.com>
2018-03-21 21:35:58 +0800

    创建、初始化和操纵数组

--- src/Array/TestArray.java
+++ src/Array/TestArray.java
@@ -3,9 +3,10 @@ package Array;
 public class TestArray {
 
 	public static void main(String[] args) {
-	      // 数组大小
+	      //数组大小
 	      int size = 10;
-	      // 定义数组
+	      
+	      //定义数组
 	      double[] myList = new double[size];
 	      myList[0] = 5.6;
 	      myList[1] = 4.5;
@@ -17,11 +18,13 @@ public class TestArray {
 	      myList[7] = 45.45;
 	      myList[8] = 99.993;
 	      myList[9] = 11123;
+	      
 	      // 计算所有元素的总和
 	      double total = 0;
 	      for (int i = 0; i < size; i++) {
 	         total += myList[i];
 	      }
+	      
 	      System.out.println("总和为： " + total);
 	}
 }
