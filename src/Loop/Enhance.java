fb9133507e5f9393b58e22f38f3a70dd6b0a07a6
greenvc <greenvc@126.com>
2018-03-23 11:13:56 +0800

    数组的增强型 for 循环

---
+++ src/Loop/Enhance.java
@@ -0,0 +1,22 @@
+package Loop;
+
+/**
+ * 数组的增强型 for 循环
+ * @author dingy
+ *
+ */
+public class Enhance {
+
+	public static void main(String[] args) {
+	      int [] numbers = {10, 20, 30, 40, 50};
+	      
+	      for(int x : numbers ) {
+	         // x 等于 30 时跳出循环
+	         if( x == 30 ) {
+	            break;
+	         }
+	         System.out.print( x );
+	         System.out.print("\n");
+	      }
+	}
+}
